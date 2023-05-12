f4a7f8eff1d63ef7b8a69b22a6be453a5d7acb5d
Жиглов Александр <sujanaxuy@gmail.com>
2023-05-12 21:48:53 +0400

    Добавьте файлы проекта.

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly AirplaneOwner[] date = new AirplaneOwner[10];
+
+        private static int LastID = 0;
+        
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(date.Take(LastID).ToArray());
+        }
+        [HttpPost]
+        public IActionResult AddAirplaneOwner([FromForm]AirplaneOwner model)
+        {
+            if (ModelState.IsValid)
+            {
+                date[LastID] = model;
+                LastID += 1;
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult AddAirplaneOwner()
+        {
+            return View();
+        }
+        public IActionResult EditAirplaneOwner([FromRoute] int id)
+        {
+            return View(date[id]);
+        }
+
+        [HttpPost]
+        public IActionResult EditAirplaneOwner([FromForm] AirplaneOwner model, [FromRoute] int id)
+        {
+            if (ModelState.IsValid)
+            {
+                date[id] = model;
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        public IActionResult GetAirplaneOwner([FromRoute] int id)
+        {
+            return View(date[id]);
+        }
+    }
+}
---
+++ WebApplication5/Models/AirplaneOwner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebApplication5.Models
+{
+    public class AirplaneOwner
+    {
+        [DisplayName("ID")]
+        public int AeroClubId { get; set; }
+
+        [DisplayName("Фамилия")]
+        public string Name { get; set; }
+
+        [DisplayName("Дата рождения")]
+        [DataType(DataType.Date)]
+        public DateTime CreationDate { get; set; }
+
+        [DisplayName("Уровень пилотирования")]
+        public string Type { get; set; }
+
+        [DisplayName("Количество самолетов")]
+        public int NumberOfPlanes { get; set; }
+
+        [DisplayName("Место жительства")]
+        public string Location { get; set; }
+    }
+}
